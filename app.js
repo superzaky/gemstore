d733ac7f4a1a2b33943cef962dc9852f71472efc
superzaky <superkytoz@gmail.com>
2016-03-19 19:40:16 +0100

    Added custom directive

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 (function(){
 	var app = angular.module('store', [ ]); //In the array you can define dependencies (other libraries we might need)
 
+	//dash in HTML translates to camelCase in JavaScript. (Dit is voor de product-title (element in index.html)
+	app.directive('productTitle', function(){
+		//returns a directive definition object (this is basically a configuration object defining how your directive will work)
+		return{
+			//here we specify the type of directive in this case 'E' (which stands for Element). We are declaring a new HTML element.
+			restrict: 'E',
+			//here we specify the url of a template we want this directive to load into the page. In this case product-title.html.
+			templateUrl: 'product-title.html'
+		};
+	});
+
 	app.controller('StoreController', function(){ //anonymous function wordt uitgevoerd wnnr de StoreController wordt aangeroepen.
 		this.products = gems;
 	});
